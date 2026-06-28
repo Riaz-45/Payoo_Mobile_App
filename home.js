15bf07a17dde3e5adab866f9061dbeb849af9812
Riaz-45 <rakibulhasanriaz1234@gmail.com>
2026-06-28 22:47:42 +0600

    added js part for cash out part

--- home.js
+++ home.js
@@ -1,6 +1,8 @@
 const validPin = 1234;
 
 
+// add money part
+
 document.getElementById('add-money-btn').addEventListener('click', function(e){
     e.preventDefault();
     // console.log('clicked')
@@ -30,4 +32,53 @@ document.getElementById('add-money-btn').addEventListener('click', function(e){
     const totalNewAvailableBalance = addAmount + availableBalance;
 
     document.getElementById('available-balance').innerText = totalNewAvailableBalance;
+})
+
+
+
+// cash out part
+
+document.getElementById('withdraw-btn').addEventListener('click', function(e){
+    e.preventDefault();
+    // console.log('withdraw clicked')
+
+    const agentNumber = document.getElementById('agent-number').value;
+    if(agentNumber.length < 11){
+        alert('Please provide a valid agent number');
+        return;
+    }
+    
+
+    const withdrawPin = parseInt(document.getElementById('withdraw-pin').value);
+    if(withdrawPin !== validPin){
+        alert('Please provide a valid pin number');
+        return;
+    }
+
+
+    const cashOutAmount = parseInt(document.getElementById('cash-out-amount').value);
+
+    const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+    // console.log(cashOutAmount, availableBalance)
+
+    const totalNewAvailableBalance = availableBalance - cashOutAmount;
+
+    // console.log(totalNewAvailableBalance);
+
+    document.getElementById('available-balance').innerText = totalNewAvailableBalance;
+
+})
+
+
+// toggling feature
+document.getElementById('addMoney-btn').addEventListener('click', function(){
+    document.getElementById('cash-out-parent').style.display = 'none';
+    
+    document.getElementById('add-money-parent').style.display = 'block';
+})
+
+document.getElementById('cash-out-btn').addEventListener('click', function(){
+    document.getElementById('add-money-parent').style.display = 'none';
+
+    document.getElementById('cash-out-parent').style.display = 'block';
 })
